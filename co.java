641cea24a36fb7499b22a7807587ccecedfb19a1
erroer404 <91798713+erroer404@users.noreply.github.com>
2021-10-02 23:15:59 +0530

    Update co.java

--- co.java
+++ co.java
@@ -35,8 +35,7 @@ class SieveOfEratosthenes
 	public static void main(String args[])
 	{
 		int n = 30;
-		System.out.print("Following are the prime numbers ");
-		System.out.println("smaller than or equal to " + n);
+		;
 		SieveOfEratosthenes g = new SieveOfEratosthenes();
 		g.sieveOfEratosthenes(n);
 	}
